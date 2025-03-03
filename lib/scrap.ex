df49db166bc22b728ef472b2e049799cf73d3800
paulo3121 <bisdusty@gmail.com>
2025-03-02 23:04:31 -0300

    scrap final

--- lib/scrap.ex
+++ lib/scrap.ex
@@ -1,12 +1,24 @@
 defmodule Scrap do
-  def getProsa do
-    response = Req.get!("http://arquivopessoa.net/textos/986")
-    # IO.inspect(response.body)
-    {:ok, document} = Floki.parse_document(response.body)
+  def getPoesia do
+    id = :rand.uniform(4544)
+    IO.puts(id)
+
+    {:ok, document} =
+      Req.get!("http://arquivopessoa.net/textos/#{id}")
+      |> Map.get(:body)
+      |> Floki.parse_document()
 
     [{_div, _class, [autor]}] = Floki.find(document, "div.autor")
     [{_h1, _class, [titulo]}] = Floki.find(document, "h1.titulo-texto")
-    [{_div, _class, lines}] = Floki.find(document, "div.texto-poesia")
-    IO.puts(lines)
+    found = Floki.find(document, "div.texto-poesia")
+    case found do
+      [] -> getPoesia()
+      [{_div, _class, children}] ->
+	linhas = Enum.map(children, fn
+	{_s, _, []} -> "\n"
+	{_s, _, [linha]} -> linha <> "\n"
+      end)
+	poesia = "#{autor}\n\n#{id}: #{titulo}\n#{linhas}"
+    end
   end
 end
